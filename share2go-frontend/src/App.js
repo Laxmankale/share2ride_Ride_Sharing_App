21b0cb422a4c7e7e3cfaa4fa5254d0d7c7110942
Laxman Kale <lakhankale888@gmail.com>
2025-09-25 00:50:35 +0530

    App.js update

--- share2go-frontend/src/App.js
+++ share2go-frontend/src/App.js
@@ -1,22 +1,5 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
-import BookingSuccess from "./pages/BookingSuccess";
-import MyBookings from "./pages/MyBookings";
-import PostRide from "./pages/PostRide";
-import DriverBookings from "./pages/DriverBookings";
-import DriverRides from "./pages/DriverRides";
-
-// Get user from localStorage
-const storedUser = localStorage.getItem("user");
-const loggedInUser = storedUser ? JSON.parse(storedUser) : null;
-const loggedInUserId = loggedInUser ? loggedInUser.id : null;
-const loggedInRole = loggedInUser ? loggedInUser.role : null;
 
 function App() {
   return (
